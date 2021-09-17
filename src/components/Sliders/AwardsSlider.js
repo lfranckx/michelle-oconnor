7383603a368bd9f016cfec92c10b1708f34136a7
Bowie <lfranckx@thinkful.com>
2021-09-17 11:31:27 -0500

    add react-icons

--- src/components/Sliders/AwardsSlider.js
+++ src/components/Sliders/AwardsSlider.js
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { AwardsData } from './AwardsData';
 
 const AwardsSlider = () => {
+    const [current, setCurrent] = useState[0];
+    // const length = slides.length;
+
     return (
         <>
             <div>
                 {AwardsData.map((img, index) => {
-                    return <img src={img.src} alt={`awards image ${index}`} key={index} width='500px'/>
+                    return <img src={img.src} alt={`awards image ${index}`} key={index} width='500px'/>;
                 })}
             </div>
         </>
