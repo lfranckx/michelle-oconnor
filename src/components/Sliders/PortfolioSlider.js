ab321d511bab1ed543856287da5995dc72f86402
Bowie <lfranckx@thinkful.com>
2021-09-18 13:00:13 -0500

    fix prev collection bug

--- src/components/Sliders/PortfolioSlider.js
+++ src/components/Sliders/PortfolioSlider.js
@@ -6,10 +6,14 @@ const PortfolioSlider = ({ data }) => {
     const [currentSlide, setCurrentSlide] = useState(0);
     const [collectionIndex, setCollectionIndex] = useState(0);
     const [currentCollection, setCurrentCollection] = useState(data[collectionIndex]);
+    const [prevCollectionIndex, setPrevCollectionIndex] = useState(7);
+    const [prevCollection, setPrevCollection] = useState(data[prevCollectionIndex]);
 
-    // console.log('current slide', currentSlide);
-    // console.log('collection', collectionIndex);
-    // console.log(`collection:`, currentCollection);
+    console.log('current slide', currentSlide);
+    console.log('current collection index', collectionIndex);
+    console.log(`collection:`, currentCollection);
+    console.log(`prev collection index`, prevCollectionIndex);
+    console.log(`prev collection`, prevCollection);
 
     const nextSlide = () => {
         // if current slide is equal to current collection length go to next collection
@@ -21,13 +25,25 @@ const PortfolioSlider = ({ data }) => {
         setCurrentSlide(currentSlide === currentCollection.length - 1 ? 0 : currentSlide + 1);
     }
 
+    const settiingPrevCollection = () => {
+        console.log('running setPrevcollection');
+
+    }
+
     const prevSlide = () => {
-        // if current slide is equal to 0 revert back to last slide of previous collection
+
         if (currentSlide === 0) {
             setCollectionIndex(collectionIndex === 0 ? 7 : collectionIndex - 1);
             setCurrentCollection(currentCollection === data[0] ? data[7] : data[collectionIndex - 1]);
+
+            setPrevCollectionIndex(prevCollectionIndex === 0 ? 7 : prevCollectionIndex - 1);
+            setPrevCollection(prevCollection === data[0] ? data[7] : data[prevCollectionIndex - 1]);
+
+            setCurrentSlide(currentSlide === 0  ? prevCollection.length - 1 : currentSlide - 1);
+
+        } else {
+            setCurrentSlide(currentSlide === 0  ? prevCollection.length - 1 : currentSlide - 1);
         }
-        setCurrentSlide(currentSlide === 0  ? currentCollection.length - 1 : currentSlide - 1);
     }
 
     if (!Array.isArray(data) || data.length <= 0) {
@@ -38,22 +54,43 @@ const PortfolioSlider = ({ data }) => {
                     <FaArrowAltCircleRight className='right-arrow arrow' onClick={nextSlide} /> 
                 </div>
             </>
-        )
+        );
     }
 
     return (
         <div id='portfolio_slider'>
             <FaArrowAltCircleLeft className='left-arrow arrow' onClick={prevSlide} />
             <FaArrowAltCircleRight className='right-arrow arrow' onClick={nextSlide} />
-            {currentCollection.map((img, index) => {
-                return (<>
-                    <div className={index === currentSlide ? 'slide active' : 'slide'}>
-                        {index === currentSlide && (<img src={img.src} alt={img.alt} key={index} width='400px'/>)}
-                    </div>
-                    <div>
 
+            {currentCollection.map((img, i) => {
+                return (
+                    <div className='slide-wrap'  key={i}>
+                        <div className={i === currentSlide ? 'slide active' : 'slide'}>
+                            {i === currentSlide && (<img src={img.src} alt={img.alt} width='400px'/>)}
+                        </div>
                     </div>
-                </>)
+                );
+            })}
+
+            {PortfolioData.map((collection, i) => {
+                return (
+                    <>
+                        {i === collectionIndex && (<div className='bottom-que' key={i}>
+                            {collection.map((img, i) => {
+                                return (
+                                    <div className='que-wrap' key={i}>
+                                        <div className={i === currentSlide ? 'slide active' : 'slide'}>
+                                            {i === currentSlide && (<img src={img.src} alt={img.alt} width='100px' />)}
+                                        </div>
+                                        <div className={'slide-que'}>
+                                            {i !== currentSlide && (<img src={img.src} alt={img.alt} width='100px' />)}
+                                        </div>
+                                    </div>
+                                )
+                            })}
+                        </div>)}
+                    </>
+                );
             })}
         </div>
     )
