c987f0938d4ee41969e617a78557c335cfed893a
Bowie <lfranckx@thinkful.com>
2021-09-20 12:49:59 -0500

    add s3 bucket urls

--- src/components/Sliders/AwardsData.js
+++ src/components/Sliders/AwardsData.js
@@ -1,22 +1,22 @@
 export const AwardsData = [
     {
-        src: '/images/awards/2016_avant_garde.jpeg',
+        src: 'https://michelleoconnor.s3.us-east-2.amazonaws.com/awards/2016_avant_garde.jpeg',
         alt: '2016 avant garde award'
     },
     {
-        src: '/images/awards/2018_texture.jpeg',
+        src: 'https://michelleoconnor.s3.us-east-2.amazonaws.com/awards/2018_texture.jpeg',
         alt: '2018 texture award'
     },
     {
-        src: '/images/awards/2019_avant_garde.jpeg',
+        src: 'https://michelleoconnor.s3.us-east-2.amazonaws.com/2019_avant_garde.jpeg',
         alt: '2019 avant garde award'
     },
     {
-        src: '/images/awards/2019_texture.jpeg',
+        src: 'https://michelleoconnor.s3.us-east-2.amazonaws.com/2019_texture.jpeg',
         alt: '2019 texture award'
     },
     {
-        src: '/images/awards/2020_texture.jpeg',
+        src: 'https://michelleoconnor.s3.us-east-2.amazonaws.com/2020_texture.jpeg',
         alt: '2020 texture award'
     }
 ];
