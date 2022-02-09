d736e206a1ce145fed8249acca33266af6a940ee
Bowie <lfranckx@thinkful.com>
2022-02-09 11:28:49 -0600

    add formik validation to contact form

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,7 +1,9 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router';
 import { Helmet } from 'react-helmet';
-import emailjs from 'emailjs-com';
+import * as emailjs from 'emailjs-com';
+import * as Yup from 'yup';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
 import '../styles/Contact.scss';
 
 
@@ -10,17 +12,44 @@ function Contact() {
         window.scrollTo(0,0);
     }, [])
 
+    const [buttonState, handleButtonState] = useState('Send');
+    const [buttonDisabled, handleButtonDisabled] = useState(false);
+    const [message, handleMessage] = useState('');
     const history = useHistory();
 
-    function sendEmail(e) {
-        e.preventDefault();
+    const submitForm = (values) => {
+        handleButtonState('Sending...');
+        const newValues = {
+            email: values.email,
+            message: values.message,
+            name: values.name,
+            phone: values.phone,
+            website: values.website,
+            salon: values.salon,
+            reply_to: values.email
+        }
 
-        emailjs.sendForm('service_rrj1goy', 'template_lc3rglk', e.target, 'user_4ZnH44kohKcJmQhnL2VGX')
+        try {
+            emailjs.send("service_rrj1goy", "template_lc3rglk", newValues, "user_4ZnH44kohKcJmQhnL2VGX")
             .then(res => {
-                history.push('/thankyou');
-            })
-            .catch(err => console.log(err));
+                    handleButtonState('Sent');
+                    handleButtonDisabled(true);
+                    history.push('/thankyou');
+                }
+            )
+        } catch (error) {
+            handleMessage(error.message);
+        }
     }
+
+    const phoneRegExp = /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/
+    const contactFormSchema = Yup.object().shape({
+        name: Yup.string().min(2, '* Name is too short').max(20, "* 20 maximum characters").required('* Required'),
+        email: Yup.string().email("* This is not an email").required("* Required"),
+        phone: Yup.string().matches(phoneRegExp, `* This doesn't look like a phone number`).max(10, '* Phone number is too long').required("* Required"),
+        message: Yup.string().min(2, "* Message is too short").max(120, "* 120 maximum characters").required("* Required"),
+        website: Yup.string().min(2, "* Sorry, this is too short").max(50, "* 50 maximum characters")
+    })
 
     return (
         <>
@@ -38,71 +67,87 @@ function Contact() {
                     </div>
                     <div className='right-wrap'>
                         <h1>Get in touch</h1>
-                        <form onSubmit={sendEmail}>
-                            <div className="label">
-                                <label htmlFor="name"></label>
-                            </div>
-                            <div>
-                                <input
-                                    type="text"
-                                    name="name"
-                                    placeholder='Your Name'
-                                />
-                            </div>
+                        <Formik
+                            initialValues={{ name: "", email: "", phone: "", message: "", website: "", salon: "" }} 
+                            validationSchema={contactFormSchema}
+                            onSubmit={submitForm}
+                        >
+                            <Form>
+                                <div className="label">
+                                    <label htmlFor="name"></label>
+                                </div>
+                                <div>
+                                    <Field
+                                        name="name"
+                                        placeholder='Your Name'
+                                    />
+                                </div>
+                                <div>
+                                    <ErrorMessage component="div" className='error' name='name' />
+                                </div>
 
-                            <div className="label">
-                                <label htmlFor="salon"></label>
-                            </div>
+                                <div className="label">
+                                    <label htmlFor="email"></label>
+                                </div>
+                                <div>
+                                    <Field
+                                        name="email"
+                                        placeholder="Email"
+                                    />
+                                </div>
+                                <div>
+                                    <ErrorMessage component="div" className='error' name='email' />
+                                </div>
 
-                            <div className="label">
-                                <label htmlFor="email"></label>
-                            </div>
-                            <div>
-                                <input
-                                    type="email"
-                                    name="email"
-                                    placeholder="Email"
-                                />
-                            </div>
+                                <div className="label">
+                                    <label htmlFor="phone"></label>
+                                </div>
+                                <div>
+                                    <Field
+                                        name="phone"
+                                        placeholder='Your Phone Number'
+                                    />
+                                </div>
+                                <div>
+                                    <ErrorMessage component="div" className='error' name='phone' />
+                                </div>
 
-                            <div className="label">
-                                <label htmlFor="phone"></label>
-                            </div>
-                            <div>
-                                <input
-                                    type="tel"
-                                    name="phone"
-                                    placeholder='Your Phone Number'
-                                />
-                            </div>
+                                <div className="label">
+                                    <label htmlFor="website"></label>
+                                </div>
+                                <div>
+                                    <Field
+                                        name="website"
+                                        placeholder='Your Website or Instagram'
+                                    />
+                                </div>
+                                <div>
+                                    <ErrorMessage component="div" className='error' name='website' />
+                                </div>
 
-                            <div className="label">
-                                <label htmlFor="website"></label>
-                            </div>
-                            <div>
-                                <input
-                                    type="text"
-                                    name="website"
-                                    placeholder='Your Website or Instagram'
-                                />
-                            </div>
+                                <div className="label">
+                                    <label htmlFor="message"></label>
+                                </div>
 
-                            <div className="label">
-                                <label htmlFor="message"></label>
-                            </div>
+                                <div>
+                                    <Field 
+                                        as="textarea"
+                                        name='message' 
+                                        rows='10' 
+                                        placeholder="Describe how you would like to work with us"
+                                    />
+                                </div>
+                                <div>
+                                    <ErrorMessage component="div" className='error' name='message' />
+                                </div>
+                                
+                                <button className="btn" type="submit" disabled={buttonDisabled}>
+                                    {buttonState}
+                                </button>
+                            </Form>
+                        </Formik>
 
-                            <div>
-                                <textarea 
-                                    name='message' 
-                                    rows='10' 
-                                    placeholder="Describe how you would like to work with us"
-                                />
-                            </div>
-                            
-                            <button className="btn" type="submit">
-                                Send
-                            </button>
-                        </form>
+                        {message && <div className='message'>{message}</div>}
                     </div>
                 </section>
             </main>
